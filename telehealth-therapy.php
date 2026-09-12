81b3d114093f857e33de4b6115d8fa5c4b7a9392
bradtn <bradnohra@gmail.com>
2026-09-12 18:17:52 -0400

    Keep appointment link on same line as phone number in callouts

--- telehealth-therapy.php
+++ telehealth-therapy.php
@@ -101,7 +101,7 @@ $schema_json = json_encode([
 
                 <div class="callout callout--brand">
                     <h3>Start Telehealth Therapy Today</h3>
-                    <p>Ready to begin online therapy?<br><span style="white-space:nowrap"><strong>Call:</strong> <a href="tel:313-654-1915">(313) 654-1915</a></span><br>or <a href="/appointment">Request an Appointment Online</a></p>
+                    <p>Ready to begin online therapy?<br><span style="white-space:nowrap"><strong>Call:</strong> <a href="tel:313-654-1915">(313) 654-1915</a></span> or <a href="/appointment">Request an Appointment Online</a></p>
                 </div>
             </article>
 
