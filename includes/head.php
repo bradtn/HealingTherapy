93c8f398929309fe30d48f5ccde08ede4a5c5465
bradtn <bradnohra@gmail.com>
2026-09-11 23:14:31 -0400

    Center the autism screening quiz column on wide screens

--- includes/head.php
+++ includes/head.php
@@ -17,7 +17,7 @@ if (!defined('BUSINESS_NAME')) {
 if (!defined('V2_PREVIEW')) {
     define('V2_PREVIEW', false);
 }
-define('V2_CSS_VERSION', '2.7.8');
+define('V2_CSS_VERSION', '2.7.9');
 
 $og_title = $og_title ?? $page_title;
 $og_description = $og_description ?? $page_description;
